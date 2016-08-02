aa0c02a995512d398d9f9332f0b04f595c289167
dhammar <ddh@iastate.edu>
2016-08-02 07:41:34 -0500

    whitespace changes

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -2,17 +2,17 @@ import {expect} from 'chai';
 import reducer from '../src/reducer';
 
 describe('reducer', () => {
-			it('handles ADD_PLAYER', () => {
-				const initialState = {
-					players: ["ABCD"]
-				};
-				const playerCode = 'ABCDEFG';
-				const action = {type: 'ADD_PLAYER', player: playerCode};
+	it('handles ADD_PLAYER', () => {
+		const initialState = {
+			players: ["ABCD"]
+		};
+		const playerCode = 'ABCDEFG';
+		const action = {type: 'ADD_PLAYER', player: playerCode};
 
-				const nextState = reducer(initialState, action);
-				expect(nextState).to.deep.equal({
-					...nextState,
-					players: ["ABCD", "ABCDEFG"]
-				});
+		const nextState = reducer(initialState, action);
+		expect(nextState).to.deep.equal({
+			...nextState,
+			players: ["ABCD", "ABCDEFG"]
+		});
 	});
 });
