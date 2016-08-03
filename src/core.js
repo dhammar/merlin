7b46f9d568ab8080fcdcd72486248d4926c826e6
dhammar <ddh@iastate.edu>
2016-08-03 08:00:47 -0500

    ditched react immutability helper for spread, removed state

--- src/core.js
+++ src/core.js
@@ -1,47 +1,35 @@
 import update from 'react-addons-update';
 
 export function addPlayer(state, player){
-	return update(state, {
-		players: {$push: [player]}
-	});
-};
+	return {
+		...state,
+		players: [...state.players, player]
+	};	
+}; 
 
 export function selectMission(state, newMission){
-	return isValidMission(state, newMission) ? update(state, {
-		mission: {$set: newMission}
-	}) : state;
+	return isValidMission(state, newMission) ? {
+		...state,
+		mission: newMission
+	} : state;
 };
 
-export function recordVote(state, vote){
-	return update(state, alreadyVoted(state,vote) ? {
-		mission: {votes: {$apply: (v) => {
-			return v.voter == vote.voter ? vote.voter : v.voter;
-		}}}} : 
-		{mission: {votes: {$push: [vote]}}}
-	);
-};
-
-function alreadyVoted(state, vote){
-	if(state.votes){
-		state.votes.forEach((v) => {
-			if(v.voter.equals === vote.voter){
-				return true;
-			}
-		});
-	}
-	return false; 
+export function recordVote(state, payloadVote){
+	return {
+		...state,
+		mission: {
+			...state.mission,
+			votes: [...state.mission.votes.filter((v) => v.voter != payloadVote.voter), payloadVote]
+		}
+	};
 };
 
 function isValidMission(state, mission){
-	var missionSize = -2;
-
-	for (var i = 0; i < state.gameBoard.length; i++) {
-		if(state.gameBoard[i] !== -1 || 0){ // -1 signifies a mission loss, 0 signifies a win
-			missionSize = state.gameBoard[i];
-			break;
-		} 
-	};
 
 	return ((new Set(mission)).size === mission.length) && 
-		mission.length === missionSize;
-}
+		mission.length === state.gameBoard.find((x) => {
+			if(x !== -1 && x !== 0){
+				return x;
+			}
+		});
+};
